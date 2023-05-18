033331c20a6aa898dec254da8b04b3342adb1626
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-05-18 22:37:43 +0900

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -13,16 +13,6 @@ export async function getServerSideProps() {
     }
 }
 
-// export default function Home({ sceneries = [] }) {
-//     return (
-//         <Layout sceneries={sceneries}>
-//             <h1 className="text-xl font-medium text-gray-800">Top-rated places to stay</h1>
-//             <p className="text-gray-500">Explore some of the best places in the world</p>
-//             <div className="mt-8"></div>
-//         </Layout>
-//     )
-// }
-
 const Home = ({ sceneries = [] }) => {
     return (
         <Layout sceneries={sceneries}>
