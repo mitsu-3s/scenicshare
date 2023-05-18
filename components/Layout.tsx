a443a04506104e3c45fa39ad0a27f031f194e1d6
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-05-18 21:54:25 +0900

    Update Layout.tsx

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -26,44 +26,46 @@ const Layout = ({ sceneries = [] }: any) => {
                 /> */}
                 <link rel="icon" href="/favicon.ico" />
             </Head>
-            <header className="body-font bg-white text-gray-600">
-                <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
-                    <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
-                        <svg
-                            xmlns="http://www.w3.org/2000/svg"
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="h-10 w-10 rounded-full bg-green-500 p-2 text-white"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
-                        </svg>
-                        <span className="ml-3 text-xl">Tailblocks</span>
-                    </a>
-                    <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
-                        <Link href="/" legacyBehavior>
-                            <a className="mr-5 hover:text-gray-900">First Link</a>
-                        </Link>
-                    </nav>
-                    <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 px-3 py-1 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
-                        Button
-                        <svg
-                            fill="none"
-                            stroke="currentColor"
-                            strokeLinecap="round"
-                            strokeLinejoin="round"
-                            strokeWidth="2"
-                            className="ml-1 h-4 w-4"
-                            viewBox="0 0 24 24"
-                        >
-                            <path d="M5 12h14M12 5l7 7-7 7"></path>
-                        </svg>
-                    </button>
-                </div>
-            </header>
+            <div className="h-screen bg-white">
+                <header className="body-font bg-white text-gray-600 shadow-md">
+                    <div className="container mx-auto flex flex-col flex-wrap items-center p-5 md:flex-row">
+                        <a className="title-font mb-4 flex items-center font-medium text-gray-900 md:mb-0">
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="h-10 w-10 rounded-full bg-green-500 p-2 text-white"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+                            </svg>
+                            <span className="ml-3 text-xl">ScenicShare</span>
+                        </a>
+                        <nav className="flex flex-wrap items-center justify-center text-base md:ml-auto">
+                            <Link href="/" legacyBehavior>
+                                <a className="mr-5 hover:text-gray-900">Link</a>
+                            </Link>
+                        </nav>
+                        <button className="mt-4 inline-flex items-center rounded border-0 bg-gray-100 px-3 py-1 text-base hover:bg-gray-200 focus:outline-none md:mt-0">
+                            Button
+                            <svg
+                                fill="none"
+                                stroke="currentColor"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                className="ml-1 h-4 w-4"
+                                viewBox="0 0 24 24"
+                            >
+                                <path d="M5 12h14M12 5l7 7-7 7"></path>
+                            </svg>
+                        </button>
+                    </div>
+                </header>
+            </div>
         </>
     )
 }
