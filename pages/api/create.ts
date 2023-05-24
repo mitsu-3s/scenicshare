da974baa0945cb7e7b610c8ba13861a923362398
mitsu-3s <mitsuuu.s3s@gmail.com>
2023-05-24 09:01:28 +0900

    Update create.ts

--- pages/api/create.ts
+++ pages/api/create.ts
@@ -6,13 +6,13 @@ const prisma = new PrismaClient()
 export default async function handler(req: NextApiRequest, res: NextApiResponse) {
     if (req.method === 'POST') {
         try {
-            const { image, title, description, likes } = req.body
+            const { image, title, description } = req.body
             const scenery = await prisma.scenery.create({
                 data: {
                     image,
                     title,
                     description,
-                    likes,
+                    likes: 0,
                 },
             })
             res.status(200).json(scenery)
